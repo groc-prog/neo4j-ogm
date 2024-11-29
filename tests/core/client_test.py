c80894e2f3e12d685e245779466fd97d64b25424
groc-prog <marc.troisner@gmail.com>
2024-11-29 15:56:52 +0100

    test(client): add tests for constraint dropping

--- tests/core/client_test.py
+++ tests/core/client_test.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-class-docstring, redefined-outer-name, unnecessary-lambda
+# pylint: disable=missing-class-docstring, redefined-outer-name, unused-import, unused-argument
 
 from unittest.mock import AsyncMock, patch
 
@@ -11,16 +11,22 @@ from pyneo4j_ogm.exceptions import (
     ClientNotInitializedError,
     UnsupportedDatabaseVersionError,
 )
-from tests.fixtures.db import Authentication, ConnectionString
+from tests.fixtures.db import (
+    Authentication,
+    ConnectionString,
+    memgraph_client,
+    memgraph_session,
+    neo4j_client,
+    neo4j_session,
+)
 
 
-class TestNeo4jClient:
-    @pytest.mark.asyncio
+class TestNeo4jConnection:
+
     async def test_successful_connect(self):
         client = Neo4jClient()
         await client.connect(ConnectionString.NEO4J.value, auth=Authentication.NEO4J.value)
 
-    @pytest.mark.asyncio
     async def test_checks_connectivity_and_auth(self):
         async_driver_mock = AsyncMock(spec=neo4j.AsyncDriver)
 
@@ -32,7 +38,6 @@ class TestNeo4jClient:
 
             verify_connectivity_spy.assert_called()
 
-    @pytest.mark.asyncio
     async def test_validates_neo4j_version(self):
         server_info_mock = AsyncMock()
         server_info_mock.agent = "Neo4j/4.9.0"
@@ -42,19 +47,16 @@ class TestNeo4jClient:
                 client = Neo4jClient()
                 await client.connect(ConnectionString.NEO4J.value, auth=Authentication.NEO4J.value)
 
-    @pytest.mark.asyncio
     async def test_raises_on_invalid_uri(self):
         with pytest.raises(ValueError):
             client = Neo4jClient()
             await client.connect("bolt://invalid-connection:9999")
 
-    @pytest.mark.asyncio
     async def test_raises_on_invalid_auth(self):
         with pytest.raises(AuthError):
             client = Neo4jClient()
             await client.connect(ConnectionString.NEO4J.value)
 
-    @pytest.mark.asyncio
     async def test_is_chainable(self):
         client = Neo4jClient()
         chainable = await client.connect(ConnectionString.NEO4J.value, auth=Authentication.NEO4J.value)
@@ -62,7 +64,6 @@ class TestNeo4jClient:
         assert isinstance(chainable, Neo4jClient)
         assert chainable == client
 
-    @pytest.mark.asyncio
     async def test_connected_state(self):
         client = Neo4jClient()
 
@@ -73,7 +74,6 @@ class TestNeo4jClient:
         is_connected = await client.connected()
         assert is_connected is True
 
-    @pytest.mark.asyncio
     async def test_connected_on_connection_failure(self):
         async_driver_mock = AsyncMock(spec=neo4j.AsyncDriver)
 
@@ -88,14 +88,12 @@ class TestNeo4jClient:
             is_connected = await client.connected()
             assert is_connected is False
 
-    @pytest.mark.asyncio
     async def test_close_raises_if_not_initialized(self):
         client = Neo4jClient()
 
         with pytest.raises(ClientNotInitializedError):
             await client.close()
 
-    @pytest.mark.asyncio
     async def test_closes_driver_connection(self):
         async_driver_mock = AsyncMock(spec=neo4j.AsyncDriver)
 
@@ -108,13 +106,42 @@ class TestNeo4jClient:
             assert client._driver is None
 
 
-class TestMemgraphClient:
-    @pytest.mark.asyncio
+class TestNeo4jConstraints:
+    async def _setup_constraints(self, session: neo4j.AsyncSession):
+        await session.run("CREATE CONSTRAINT book_isbn FOR (book:Book) REQUIRE book.isbn IS UNIQUE")
+        await session.run("CREATE CONSTRAINT sequels FOR ()-[sequel:SEQUEL_OF]-() REQUIRE sequel.order IS UNIQUE")
+        await session.run(
+            "CREATE CONSTRAINT book_title_year FOR (book:Book) REQUIRE (book.title, book.publicationYear) IS UNIQUE"
+        )
+        await session.run(
+            "CREATE CONSTRAINT prequels FOR ()-[prequel:PREQUEL_OF]-() REQUIRE (prequel.order, prequel.author) IS UNIQUE"
+        )
+
+        query = await session.run("SHOW CONSTRAINT")
+        constraints = await query.values()
+        assert len(constraints) == 4
+
+    async def test_drop_constraints(self, neo4j_session, neo4j_client):
+        await self._setup_constraints(neo4j_session)
+        await neo4j_client.drop_constraints()
+
+        query = await neo4j_session.run("SHOW CONSTRAINT")
+        constraints = await query.values()
+        assert len(constraints) == 0
+
+    async def test_does_nothing_if_no_constraints_defined(self, neo4j_session, neo4j_client):
+        with patch("pyneo4j_ogm.core.client.Neo4jClient.cypher", wraps=neo4j_client.cypher) as cypher_spy:
+            await neo4j_client.drop_constraints()
+
+            cypher_spy.assert_called_once()
+
+
+class TestMemgraphConnection:
+
     async def test_successful_connect(self):
         client = MemgraphClient()
         await client.connect(ConnectionString.MEMGRAPH.value, auth=Authentication.MEMGRAPH.value)
 
-    @pytest.mark.asyncio
     async def test_checks_connectivity_and_auth(self):
         async_driver_mock = AsyncMock(spec=neo4j.AsyncDriver)
 
@@ -126,19 +153,16 @@ class TestMemgraphClient:
 
             verify_connectivity_spy.assert_called()
 
-    @pytest.mark.asyncio
     async def test_raises_on_invalid_uri(self):
         with pytest.raises(ValueError):
             client = MemgraphClient()
             await client.connect("bolt://invalid-connection:9999")
 
-    @pytest.mark.asyncio
     async def test_raises_on_invalid_auth(self):
         with pytest.raises(ClientError):
             client = MemgraphClient()
             await client.connect(ConnectionString.MEMGRAPH.value)
 
-    @pytest.mark.asyncio
     async def test_is_chainable(self):
         client = MemgraphClient()
         chainable = await client.connect(ConnectionString.MEMGRAPH.value, auth=Authentication.MEMGRAPH.value)
@@ -146,7 +170,6 @@ class TestMemgraphClient:
         assert isinstance(chainable, MemgraphClient)
         assert chainable == client
 
-    @pytest.mark.asyncio
     async def test_connected_state(self):
         client = MemgraphClient()
 
@@ -157,7 +180,6 @@ class TestMemgraphClient:
         is_connected = await client.connected()
         assert is_connected is True
 
-    @pytest.mark.asyncio
     async def test_connected_on_connection_failure(self):
         async_driver_mock = AsyncMock(spec=neo4j.AsyncDriver)
 
@@ -172,14 +194,12 @@ class TestMemgraphClient:
             is_connected = await client.connected()
             assert is_connected is False
 
-    @pytest.mark.asyncio
     async def test_close_raises_if_not_initialized(self):
         client = MemgraphClient()
 
         with pytest.raises(ClientNotInitializedError):
             await client.close()
 
-    @pytest.mark.asyncio
     async def test_closes_driver_connection(self):
         async_driver_mock = AsyncMock(spec=neo4j.AsyncDriver)
 
@@ -190,3 +210,27 @@ class TestMemgraphClient:
             await client.close()
             close_spy.assert_called()
             assert client._driver is None
+
+
+class TestMemgraphConstraints:
+    async def _setup_constraints(self, session: neo4j.AsyncSession):
+        await session.run("CREATE CONSTRAINT ON (n:Employee) ASSERT n.id IS UNIQUE")
+        await session.run("CREATE CONSTRAINT ON (n:Employee) ASSERT n.name, n.address IS UNIQUE")
+
+        query = await session.run("SHOW CONSTRAINT INFO")
+        constraints = await query.values()
+        assert len(constraints) == 2
+
+    async def test_drop_constraints(self, memgraph_session, memgraph_client):
+        await self._setup_constraints(memgraph_session)
+        await memgraph_client.drop_constraints()
+
+        query = await memgraph_session.run("SHOW CONSTRAINT INFO")
+        constraints = await query.values()
+        assert len(constraints) == 0
+
+    async def test_does_nothing_if_no_constraints_defined(self, memgraph_session, memgraph_client):
+        with patch("pyneo4j_ogm.core.client.MemgraphClient.cypher", wraps=memgraph_client.cypher) as cypher_spy:
+            await memgraph_client.drop_constraints()
+
+            cypher_spy.assert_called_once()
--- tests/fixtures/db.py
+++ tests/fixtures/db.py
@@ -7,6 +7,13 @@ from enum import Enum
 import pytest
 from neo4j import AsyncGraphDatabase
 
+from pyneo4j_ogm.core.client import MemgraphClient, Neo4jClient
+from pyneo4j_ogm.types.memgraph import (
+    MemgraphConstraintType,
+    MemgraphDataTypeMapping,
+    MemgraphIndexType,
+)
+
 
 class ConnectionString(Enum):
     NEO4J = "bolt://localhost:7687"
@@ -19,6 +26,34 @@ class Authentication(Enum):
 
 
 @pytest.fixture
+async def neo4j_client():
+    """
+    Provides a client for a Neo4j database started using docker-compose.yml.
+
+    yields:
+        client (Neo4jClient): A Neo4jClient instance which is already connected to the database.
+    """
+    client = await Neo4jClient().connect(ConnectionString.NEO4J.value, auth=Authentication.NEO4J.value)
+    yield client
+
+    await client.close()
+
+
+@pytest.fixture
+async def memgraph_client():
+    """
+    Provides a client for a Memgraph database started using docker-compose.yml.
+
+    yields:
+        client (MemgraphClient): A MemgraphClient instance which is already connected to the database.
+    """
+    client = await MemgraphClient().connect(ConnectionString.MEMGRAPH.value, auth=Authentication.MEMGRAPH.value)
+    yield client
+
+    await client.close()
+
+
+@pytest.fixture
 async def neo4j_session():
     """
     Provides a session for a Neo4j database started using docker-compose.yml. The database is cleared
@@ -28,9 +63,7 @@ async def neo4j_session():
         session (AsyncSession): A session to interaction with the database.
     """
     driver = AsyncGraphDatabase.driver(uri=ConnectionString.NEO4J.value, auth=Authentication.NEO4J.value)
-
-    async with driver.session() as session:
-        yield session
+    session = driver.session()
 
     # Clear all nodes created during the test
     await session.run("MATCH (n) DETACH DELETE n")
@@ -49,4 +82,67 @@ async def neo4j_session():
     for index in indexes:
         await session.run((f"DROP INDEX {index[1]}"))  # type: ignore
 
+    yield session
+
+    await session.close()
+    await driver.close()
+
+
+@pytest.fixture
+async def memgraph_session():
+    """
+    Provides a session for a Memgraph database started using docker-compose.yml. The database is cleared
+    after a test is done, meaning all nodes, constraints and indexes are dropped.
+
+    Yields:
+        session (AsyncSession): A session to interaction with the database.
+    """
+    driver = AsyncGraphDatabase.driver(uri=ConnectionString.MEMGRAPH.value, auth=Authentication.MEMGRAPH.value)
+    session = driver.session()
+
+    # Clear all nodes created during the test
+    await session.run("MATCH (n) DETACH DELETE n")
+
+    # Drop all constraints
+    query_result = await session.run("SHOW CONSTRAINT INFO")
+    constraints = [list(result.values()) async for result in query_result]
+
+    for constraint in constraints:
+        match constraint[0]:
+            case MemgraphConstraintType.EXISTS.value:
+                await session.run(
+                    f"DROP CONSTRAINT ON (n:{constraint[1]}) ASSERT EXISTS (n.{constraint[2]})",  # type: ignore
+                )
+            case MemgraphConstraintType.UNIQUE.value:
+                await session.run(
+                    f"DROP CONSTRAINT ON (n:{constraint[1]}) ASSERT {', '.join([f'n.{constraint_property}' for constraint_property in constraint[2]])} IS UNIQUE",  # type: ignore
+                )
+            case MemgraphConstraintType.DATA_TYPE.value:
+                # Some data types in Memgraph are returned differently that what is used when creating them
+                # Because of that we have to do some additional mapping when dropping them
+                await session.run(
+                    f"DROP CONSTRAINT ON (n:{constraint[1]}) ASSERT n.{constraint[2]} IS TYPED {MemgraphDataTypeMapping[constraint[3]]}",  # type: ignore
+                )
+
+    # Drop all indexes
+    query_result = await session.run("SHOW INDEX INFO")
+    indexes = [list(result.values()) async for result in query_result]
+
+    for index in indexes:
+        for index in indexes:
+            match index[0]:
+                case MemgraphIndexType.EDGE_TYPE.value:
+                    await session.run(f"DROP EDGE INDEX ON :{index[1]}")  # type: ignore
+                case MemgraphIndexType.EDGE_TYPE_AND_PROPERTY.value:
+                    await session.run(f"DROP EDGE INDEX ON :{index[1]}({index[2]})")  # type: ignore
+                case MemgraphIndexType.LABEL.value:
+                    await session.run(f"DROP INDEX ON :{index[1]}")  # type: ignore
+                case MemgraphIndexType.LABEL_AND_PROPERTY.value:
+                    await session.run(f"DROP INDEX ON :{index[1]}({index[2]})")  # type: ignore
+                case MemgraphIndexType.POINT.value:
+                    await session.run(f"DROP POINT INDEX ON :{index[1]}({index[2]})")  # type: ignore
+
+    yield session
+
+    await session.close()
     await driver.close()
