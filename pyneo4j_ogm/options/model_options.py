b32b6a509ca27e9bed3464363f8e9d510fb95aad
groc-prog <marc.troisner@gmail.com>
2024-12-07 21:50:33 +0100

    feat(model): add hooks typings and config merging

--- pyneo4j_ogm/options/model_options.py
+++ pyneo4j_ogm/options/model_options.py
@@ -1,34 +1,92 @@
-from typing import Callable, Dict, List, Tuple, TypedDict, Union
+from typing import Any, Callable, Dict, List, Set, Tuple, TypedDict, Union
 
+from pydantic import BaseModel
+
+from pyneo4j_ogm.pydantic import IS_PYDANTIC_V2
 from pyneo4j_ogm.types.graph import EagerFetchStrategy
+from pyneo4j_ogm.types.model import Hooks
+
+if IS_PYDANTIC_V2:
+    from pydantic import field_validator
+else:
+    from pydantic import validator
 
 HookConfiguration = Union[Callable, Tuple[bool, Callable]]
 
 
-class BaseOptions(TypedDict, total=False):
+class BaseConfig(TypedDict, total=False):
     """
-    Options shared by all models.
+    Configuration options shared by all models.
     """
 
-    pre_hooks: Dict[str, Union[HookConfiguration, List[HookConfiguration]]]
-    post_hooks: Dict[str, Union[HookConfiguration, List[HookConfiguration]]]
+    pre_hooks: Dict[Hooks, Union[Callable, List[Callable]]]
+    post_hooks: Dict[Hooks, Union[Callable, List[Callable]]]
     skip_constraint_creation: bool
     skip_index_creation: bool
     eager_fetch: bool
     eager_fetch_strategy: EagerFetchStrategy
 
 
-class NodeOptions(BaseOptions):
+class NodeConfig(BaseConfig, total=False):
     """
-    Options for node models used to control behavior and trigger side effects
+    Configuration options for node models used to control behavior and trigger side effects
     """
 
     labels: Union[str, List[str]]
 
 
-class RelationshipOptions(BaseOptions):
+class RelationshipConfig(BaseConfig, total=False):
     """
-    Options for relationship models used to control behavior and trigger side effects
+    Configuration options for relationship models used to control behavior and trigger side effects
     """
 
     type: str
+
+
+class ModelConfigurationValidator(BaseModel):
+    """
+    Validation model for Node/Relationship model options
+    """
+
+    pre_hooks: Dict[Hooks, Union[Callable, List[Callable]]]
+    post_hooks: Dict[Hooks, Union[Callable, List[Callable]]]
+    skip_constraint_creation: bool
+    skip_index_creation: bool
+    eager_fetch: bool
+    eager_fetch_strategy: EagerFetchStrategy
+    labels: Set[str]
+    type: str
+
+    if IS_PYDANTIC_V2:
+
+        @field_validator("pre_hooks", "post_hooks")
+        @classmethod
+        def normalize_hooks_pydantic_v2(cls, value: Any):
+            if not isinstance(value, dict):
+                raise ValueError("Hooks must be a dictionary")
+
+            normalized = {}
+            for key, hooks in value.items():
+                if isinstance(hooks, list):
+                    normalized[key] = [hook for hook in hooks if callable(hook)]
+                elif callable(hooks):
+                    normalized[key] = [hooks]
+
+            return normalized
+
+    else:
+
+        @validator("pre_hooks", "post_hooks")
+        @classmethod
+        def normalize_hooks_pydantic_v1(cls, value: Any):
+            if not isinstance(value, dict):
+                raise ValueError("Hooks must be a dictionary")
+
+            normalized = {}
+            for key, hooks in value.items():
+                if isinstance(hooks, list):
+                    normalized[key] = [hook for hook in hooks if callable(hook)]
+                elif callable(hooks):
+                    normalized[key] = [hooks]
+
+            return normalized
---
+++ pyneo4j_ogm/types/model.py
@@ -0,0 +1,18 @@
+from typing import Literal, Union
+
+Hooks = Union[
+    str,
+    Literal["create"],
+    Literal["update"],
+    Literal["delete"],
+    Literal["refresh"],
+    Literal["find_one"],
+    Literal["find_many"],
+    Literal["update_one"],
+    Literal["update_many"],
+    Literal["delete_one"],
+    Literal["delete_many"],
+    Literal["count"],
+    Literal["start_node"],
+    Literal["end_node"],
+]
