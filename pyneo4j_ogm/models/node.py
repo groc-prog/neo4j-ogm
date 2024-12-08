661d833665ab46f1898493c8ac64612fa0b9b310
groc-prog <marc.troisner@gmail.com>
2024-12-08 22:06:26 +0100

    feat(model): merge labels and use default labels if not defined

--- pyneo4j_ogm/models/node.py
+++ pyneo4j_ogm/models/node.py
@@ -1,2 +1,29 @@
-class NodeModel:
-    pass
+import re
+from copy import copy
+from typing import ClassVar, Set, cast
+
+from pyneo4j_ogm.models.base import ModelBase
+from pyneo4j_ogm.options.model_options import NodeConfig
+
+
+class NodeModel(ModelBase):
+    """
+    Base model for all node models. Every node model should inherit from this class to define a
+    model.
+    """
+
+    ogm_config: ClassVar[NodeConfig]
+
+    def __init_subclass__(cls, **kwargs):
+        # We get the labels defined in the config here since the config might get merged with
+        # some other values when calling super().__init_subclass__()
+        defined_labels = set() if "labels" not in cls.ogm_config else copy(cls.ogm_config["labels"])
+        super().__init_subclass__(**kwargs)
+
+        # If no labels are explicitly defined, we generate some from the class name
+        if len(defined_labels) == 0:
+            if "labels" not in cls.ogm_config:
+                raise ValueError("labels property not initialized")
+
+            defined_labels = re.findall(r"[A-Z][a-z]*|[A-Z]+(?=[A-Z][a-z]|$)", cls.__name__)
+            cast(Set[str], cls.ogm_config["labels"]).update(defined_labels)
--- pyneo4j_ogm/models/relationship.py
+++ pyneo4j_ogm/models/relationship.py
@@ -1,2 +1,13 @@
-class RelationshipModel:
-    pass
+from typing import ClassVar
+
+from pyneo4j_ogm.models.base import ModelBase
+from pyneo4j_ogm.options.model_options import RelationshipConfig
+
+
+class RelationshipModel(ModelBase):
+    """
+    Base model for all relationship models. Every relationship model should inherit from this class to have needed base
+    functionality like de-/inflation and validation.
+    """
+
+    ogm_config: ClassVar[RelationshipConfig]
