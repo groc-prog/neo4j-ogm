5e5f2186da9e6ccda8ac0ab24e4de4fb3361fbb3
groc-prog <marc.troisner@gmail.com>
2025-02-14 15:44:36 +0100

    test(model): tests for node create method

--- tests/models/node_test.py
+++ tests/models/node_test.py
@@ -1,7 +1,49 @@
-# pylint: disable=missing-class-docstring
+# pylint: disable=missing-class-docstring, unused-import, redefined-outer-name
 
+import json
+from types import NoneType
+from typing import Any, Dict, List, Tuple
+
+import neo4j.graph
+import pytest
+from pydantic import BaseModel
+
+from pyneo4j_ogm.clients.neo4j import Neo4jClient
+from pyneo4j_ogm.exceptions import DeflationError, EntityAlreadyCreatedError
 from pyneo4j_ogm.models.node import Node
 from pyneo4j_ogm.types.model import EagerFetchStrategy
+from tests.fixtures.db import (
+    Authentication,
+    ConnectionString,
+    memgraph_client,
+    memgraph_session,
+    neo4j_client,
+    neo4j_session,
+)
+from tests.fixtures.registry import reset_registry_state
+
+
+class NestedModel(BaseModel):
+    str_field: str
+
+
+class SimpleNode(Node):
+    str_field: str
+    bool_field: bool
+    int_field: int
+    float_field: float
+    tuple_field: Tuple[int, ...]
+    list_field: List[int]
+
+    ogm_config = {"labels": ["SimpleNode"]}
+
+
+class NestedNode(Node):
+    not_nested: str
+    nested_once: Dict[str, Any]
+    nested_model: NestedModel
+
+    ogm_config = {"labels": ["NestedNode"]}
 
 
 class TestConfiguration:
@@ -130,3 +172,193 @@ class TestConfiguration:
         assert Person._ogm_config.skip_index_creation is True  # type: ignore
         assert Person._ogm_config.eager_fetch is True  # type: ignore
         assert Person._ogm_config.eager_fetch_strategy == EagerFetchStrategy.AS_SPLIT_QUERY  # type: ignore
+
+
+class TestCreate:
+    async def test_raises_if_already_hydrated(self, neo4j_session, neo4j_client):
+        await neo4j_client.register_models(SimpleNode)
+
+        node = SimpleNode(
+            str_field="my_str",
+            bool_field=True,
+            int_field=4,
+            float_field=1.243,
+            tuple_field=tuple([1, 2, 3]),
+            list_field=[1, 2, 3],
+        )
+        await node.create()
+
+        with pytest.raises(EntityAlreadyCreatedError):
+            await node.create()
+
+        query = await neo4j_session.run(
+            "MATCH (n:NestedNode) WHERE elementId(n) = $element_id RETURN n", {"element_id": node.element_id}
+        )
+        result = await query.values()
+        await query.consume()
+
+        assert len(result) == 0
+
+    class TestWithNeo4jClient:
+        async def test_create_node(self, neo4j_session, neo4j_client):
+            await neo4j_client.register_models(SimpleNode)
+
+            query = await neo4j_session.run("MATCH (n:SimpleNode) RETURN n")
+            result = await query.values()
+            await query.consume()
+
+            assert len(result) == 0
+
+            node = SimpleNode(
+                str_field="my_str",
+                bool_field=True,
+                int_field=4,
+                float_field=1.243,
+                tuple_field=tuple([1, 2, 3]),
+                list_field=[1, 2, 3],
+            )
+
+            assert node.element_id is None
+            assert node.id is None
+            assert node.graph is None
+            assert len(node.modified_fields) == 0
+
+            await node.create()
+
+            assert node.element_id is not None
+            assert node.id is not None
+            assert node.graph is not None
+            assert len(node.modified_fields) == 0
+
+            query = await neo4j_session.run("MATCH (n:SimpleNode) RETURN n")
+            result = await query.values()
+            await query.consume()
+
+            assert len(result) == 1
+            assert isinstance(result[0][0], neo4j.graph.Node)
+
+            properties = dict(result[0][0])
+            assert result[0][0].id == node.id
+            assert result[0][0].element_id == node.element_id
+            assert properties["str_field"] == node.str_field
+            assert properties["bool_field"] == node.bool_field
+            assert properties["int_field"] == node.int_field
+            assert properties["float_field"] == node.float_field
+            assert properties["tuple_field"] == list(node.tuple_field)
+            assert properties["list_field"] == node.list_field
+
+        async def test_create_nested_node(self, neo4j_session, neo4j_client):
+            await neo4j_client.register_models(NestedNode)
+
+            node = NestedNode(
+                not_nested="not_nested", nested_once={"nested": True}, nested_model=NestedModel(str_field="str_field")
+            )
+            await node.create()
+
+            query = await neo4j_session.run(
+                "MATCH (n:NestedNode) WHERE elementId(n) = $element_id RETURN n", {"element_id": node.element_id}
+            )
+            result = await query.values()
+            await query.consume()
+
+            assert len(result) == 1
+            assert isinstance(result[0][0], neo4j.graph.Node)
+
+            properties = dict(result[0][0])
+            assert result[0][0].id == node.id
+            assert result[0][0].element_id == node.element_id
+            assert properties["not_nested"] == "not_nested"
+            assert properties["nested_once"] == json.dumps(node.nested_once)
+            assert properties["nested_model"] == json.dumps(node.nested_model.model_dump())
+
+        async def test_create_raises_if_nested_properties_not_allowed(self, neo4j_session):
+            client = Neo4jClient()
+            await client.connect(
+                ConnectionString.NEO4J.value, auth=Authentication.NEO4J.value, allow_nested_properties=False
+            )
+            await client.register_models(NestedNode)
+
+            node = NestedNode(
+                not_nested="not_nested", nested_once={"nested": True}, nested_model=NestedModel(str_field="str_field")
+            )
+
+            with pytest.raises(DeflationError):
+                await node.create()
+
+            query = await neo4j_session.run(
+                "MATCH (n:NestedNode) WHERE elementId(n) = $element_id RETURN n", {"element_id": node.element_id}
+            )
+            result = await query.values()
+            await query.consume()
+
+            assert len(result) == 0
+
+    class TestWithMemgraphClient:
+        async def test_create_node(self, memgraph_session, memgraph_client):
+            await memgraph_client.register_models(SimpleNode)
+
+            query = await memgraph_session.run("MATCH (n:SimpleNode) RETURN n")
+            result = await query.values()
+            await query.consume()
+
+            assert len(result) == 0
+
+            node = SimpleNode(
+                str_field="my_str",
+                bool_field=True,
+                int_field=4,
+                float_field=1.243,
+                tuple_field=tuple([1, 2, 3]),
+                list_field=[1, 2, 3],
+            )
+
+            assert node.element_id is None
+            assert node.id is None
+            assert node.graph is None
+            assert len(node.modified_fields) == 0
+
+            await node.create()
+
+            assert node.element_id is not None
+            assert node.id is not None
+            assert node.graph is not None
+            assert len(node.modified_fields) == 0
+
+            query = await memgraph_session.run("MATCH (n:SimpleNode) RETURN n")
+            result = await query.values()
+            await query.consume()
+
+            assert len(result) == 1
+            assert isinstance(result[0][0], neo4j.graph.Node)
+
+            properties = dict(result[0][0])
+            assert result[0][0].id == node.id
+            assert result[0][0].element_id == node.element_id
+            assert properties["str_field"] == node.str_field
+            assert properties["bool_field"] == node.bool_field
+            assert properties["int_field"] == node.int_field
+            assert properties["float_field"] == node.float_field
+            assert properties["tuple_field"] == list(node.tuple_field)
+            assert properties["list_field"] == node.list_field
+
+        async def test_create_nested_node(self, memgraph_session, memgraph_client):
+            await memgraph_client.register_models(NestedNode)
+
+            node = NestedNode(
+                not_nested="not_nested", nested_once={"nested": True}, nested_model=NestedModel(str_field="str_field")
+            )
+            await node.create()
+
+            query = await memgraph_session.run("MATCH (n:NestedNode) WHERE id(n) = $id RETURN n", {"id": node.id})
+            result = await query.values()
+            await query.consume()
+
+            assert len(result) == 1
+            assert isinstance(result[0][0], neo4j.graph.Node)
+
+            properties = dict(result[0][0])
+            assert result[0][0].id == node.id
+            assert result[0][0].element_id == node.element_id
+            assert properties["not_nested"] == "not_nested"
+            assert properties["nested_once"] == node.nested_once
+            assert properties["nested_model"] == node.nested_model.model_dump()
